4ef1bcd6bfa719aa629ded3bf0ac004ecdb02a7f
Kundan Singh <kk45663@gmail.com>
2015-01-21 16:49:54 +0530

    second change

--- HelloCheck/src/com/example/hellocheck/MainActivity.java
+++ HelloCheck/src/com/example/hellocheck/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //hello world example
     }
 }
